f9f76cd4724437a0c2a4d1701aa9fae0ccc84004
Goku <johnortega@bellsouth.net>
2019-05-12 00:07:09 -0400

    ui updates

--- config/connection.js
+++ config/connection.js
@@ -14,6 +14,6 @@ if(process.env.JAWSDB_URL){
 
 }
 
-connection.connect();
+// connection.connect();
 
 module.exports = connection;
--- public/assets/js/bugerpage.js
+++ public/assets/js/bugerpage.js
@@ -1,5 +1,6 @@
 
 
+
 $(document).on("click",".devoure", function(event) {
     var id = $(this).data("id");
     console.log('devoure2:', id)
@@ -22,6 +23,10 @@ $(document).on("click",".devoure", function(event) {
 
 $(document).on("click",".submit", function(event) {
     event.preventDefault();
+    let null_check = $("#burg").val().trim();
+    if(null_check != ""){
+
+    
 
     var newBurger = {
         burger_name: $("#burg").val().trim(),
@@ -39,6 +44,8 @@ $(document).on("click",".submit", function(event) {
           // Reload the page to get the updated list
           location.reload();
         });
-
+      }else{
+        console.log("Nothing Entered")
+      }
 });
 
